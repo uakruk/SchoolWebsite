1614bb21eb3718b03153599c1be3fed86f8c7492
Yaroslav Kruk <yakruck@gmail.com>
2016-12-08 00:56:48 +0200

    Initialized application

---
+++ src/main/java/ua/edu/kordelschool/KodreslchoolApplication.java
@@ -0,0 +1,12 @@
+package ua.edu.kordelschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodreslchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodreslchoolApplication.class, args);
+	}
+}
---
+++ src/test/java/ua/edu/kordelschool/KodreslchoolApplicationTests.java
@@ -0,0 +1,16 @@
+package ua.edu.kordelschool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KodreslchoolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
