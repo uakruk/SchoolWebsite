c4bb5087b12a08089310261ffbc1fc6de4e937ad
AleksandrPea <pealeks@ukr.net>
2016-12-20 18:34:12 +0200

    createArticleTest has been fixed and is working

--- src/test/java/ua/edu/kordelschool/controller/ArticlesEndpointTest.java
+++ src/test/java/ua/edu/kordelschool/controller/ArticlesEndpointTest.java
@@ -1,66 +1,104 @@
 package ua.edu.kordelschool.controller;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.mock.http.MockHttpOutputMessage;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestContext;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import ua.edu.kordelschool.dto.ArticleDto;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import ua.edu.kordelschool.KodreslchoolApplication;
 import ua.edu.kordelschool.entity.Article;
+import ua.edu.kordelschool.entity.Attachment;
+import ua.edu.kordelschool.entity.AttachmentType;
 import ua.edu.kordelschool.service.ArticleService;
 
-import static junit.framework.Assert.assertNull;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.*;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.GregorianCalendar;
+import java.util.HashSet;
+
+import static org.junit.Assert.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {TestContext.class, WebAppContext.class})
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = KodreslchoolApplication.class)
 @WebAppConfiguration
 public class ArticlesEndpointTest {
 
-    @Autowired
+    private MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(),
+            Charset.forName("utf8"));
+
     private MockMvc mockMvc;
 
+    private HttpMessageConverter mappingJackson2HttpMessageConverter;
+
     @Autowired
-    private ArticleService articleServiceMock;
+    private ArticleService articleService;
 
-    //Add WebApplicationContext field here.
+    @Autowired
+    private WebApplicationContext webApplicationContext;
 
-    //The setUp() method is omitted.
+    @Autowired
+    void setConverters(HttpMessageConverter<?>[] converters) {
+
+        this.mappingJackson2HttpMessageConverter = Arrays.asList(converters).stream()
+                .filter(hmc -> hmc instanceof MappingJackson2HttpMessageConverter)
+                .findAny()
+                .orElse(null);
+
+        assertNotNull("the JSON message converter must not be null",
+                this.mappingJackson2HttpMessageConverter);
+    }
+
+    @Before
+    public void setup() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
 
     @Test
     public void createArticleTest() throws Exception {
-        ArticleDto dto = new ArticleDto();
-        dto.setAuthor("author");
-        dto.setCaption("caption");
+        Article article = new Article();
+        article.setCaption("caption");
+        article.setAuthor("author");
+        article.setText("text");
+        article.setId(0L);
+        article.setDate(new GregorianCalendar());
+        HashSet<Attachment> attachments = new HashSet<>();
+        Attachment attachment = new Attachment("someGoogle.img", AttachmentType.IMAGE);
+        attachment.setId(0L);
+        attachments.add(attachment);
+        article.setAttachments(attachments);
+        String articleJson = json(article);
 
-        Article added = new Article();
-        dto.setAuthor("author");
-        dto.setCaption("caption");
+        mockMvc.perform(post("/article/add")
+                .contentType(contentType)
+                .content(articleJson))
+                .andExpect(status().isOk());
+    }
 
-        when(articleServiceMock.createArticle(any(ArticleDto.class))).thenReturn(added);
+    @Test
+    public void editArticleTest() {
 
-        mockMvc.perform(post("/article/add"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.author", is("author")))
-                .andExpect(jsonPath("$.caption", is("caption")));
+    }
 
-        ArgumentCaptor<ArticleDto> dtoCaptor = ArgumentCaptor.forClass(ArticleDto.class);
-        verify(articleServiceMock, times(1)).createArticle(dtoCaptor.capture());
-        verifyNoMoreInteractions(articleServiceMock);
-
-        ArticleDto dtoArgument = dtoCaptor.getValue();
-        assertNull(dtoArgument.getId());
-        assertThat(dtoArgument.getAuthor(), is("author"));
-        assertThat(dtoArgument.getCaption(), is("caption"));
+    protected String json(Object o) throws IOException {
+        MockHttpOutputMessage mockHttpOutputMessage = new MockHttpOutputMessage();
+        this.mappingJackson2HttpMessageConverter.write(
+                o, MediaType.APPLICATION_JSON, mockHttpOutputMessage);
+        return mockHttpOutputMessage.getBodyAsString();
     }
 }
